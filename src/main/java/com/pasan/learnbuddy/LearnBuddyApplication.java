fa93ca174eeabb1d74fe0072a5783a21d1db1f12
duppasan18 <282635095@qq.com>
2026-03-05 21:39:32 +0800

    完成了jwt工具类的编写

--- src/main/java/com/pasan/learnbuddy/LearnBuddyApplication.java
+++ src/main/java/com/pasan/learnbuddy/LearnBuddyApplication.java
@@ -5,6 +5,7 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.core.env.Environment;
 
 import java.net.InetAddress;
@@ -13,6 +14,7 @@ import java.net.UnknownHostException;
 @SpringBootApplication
 @MapperScan("com.pasan.learnbuddy.mapper")
 @Slf4j
+@EnableConfigurationProperties
 public class LearnBuddyApplication {
 
     public static void main(String[] args) throws UnknownHostException {
