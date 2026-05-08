3e9620372c0921bd82a07aad2ec9a42a70adfb29
duppasan18 <282635095@qq.com>
2026-05-08 17:48:29 +0800

    fix(user):为测试用登录接口添加了白名单 refactor(common):修改了user模块使用的redis key常量
    - 修改了user模块使用的redis key常量，使项目命名更统一
    - 修复了测试用登录接口无法使用的bug

--- lb-common/src/main/java/com/pasan/constants/RedisConstant.java
+++ lb-common/src/main/java/com/pasan/constants/RedisConstant.java
@@ -4,6 +4,9 @@ public class RedisConstant {
     public static final String USER_LOCATION_KEY = "user:location"; // 用户位置
     public static final String NEARBY_MEMBER_PREFIX = "user:";  // 用户前缀
     public static final String TOKEN_KEY = "user:token:" ; // token前缀
+    public static final String USER_LOCATION_KEY = "user:geo"; // 用户位置
+    public static final String USER_PREFIX = "user:";  // 用户前缀
+    public static final String USER_TOKEN_KEY_PREFIX = "user:token:" ; // token前缀
 
     public static final String ROOM_LOCATION_KEY = "room:geo"; // 自习室位置
     public static final String ROOM_INFO_KEY = "room:info"; // 自习室信息
--- lb-common/src/main/java/com/pasan/filter/JwtAuthenticationFilter.java
+++ lb-common/src/main/java/com/pasan/filter/JwtAuthenticationFilter.java
@@ -11,13 +11,10 @@ import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
