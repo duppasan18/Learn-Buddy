6c1d0fda54c89f7be25b5e1420c9c037f73de013
duppasan18 <282635095@qq.com>
2026-05-08 22:13:48 +0800

    refactor(common):重命名部分redis key常量
    - 重命名部分redis key常量，使其更加规范

--- lb-common/src/main/java/com/pasan/constants/RedisConstant.java
+++ lb-common/src/main/java/com/pasan/constants/RedisConstant.java
@@ -1,20 +1,18 @@
 package com.pasan.constants;
 
 public class RedisConstant {
-    public static final String USER_LOCATION_KEY = "user:location"; // 用户位置
-    public static final String NEARBY_MEMBER_PREFIX = "user:";  // 用户前缀
-    public static final String TOKEN_KEY = "user:token:" ; // token前缀
     public static final String USER_LOCATION_KEY = "user:geo"; // 用户位置
     public static final String USER_PREFIX = "user:";  // 用户前缀
     public static final String USER_TOKEN_KEY_PREFIX = "user:token:" ; // token前缀
 
     public static final String ROOM_LOCATION_KEY = "room:geo"; // 自习室位置
-    public static final String ROOM_INFO_KEY = "room:info"; // 自习室信息
+    public static final String ROOM_INFO_KEY_PREFIX = "room:info"; // 自习室信息
 
-    public static final String INVITATION_COUNT_KEY = "invite:count:"; // 邀约当前人数
+    public static final String INVITATION_COUNT_KEY_PREFIX = "invite:count:"; // 邀约当前人数
     public static final String INVITATION_LOCATION_KEY = "invite:geo"; // 邀约自习室位置
     public static final String INVITATION_INFO_KEY = "invite:info:"; // 邀约信息
 
+    public static final String INVITATION_INFO_KEY_PREFIX = "invite:info:"; // 邀约信息
 
 
 
